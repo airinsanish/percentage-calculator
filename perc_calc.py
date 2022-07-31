461bbaa2bf58e9e52617307041d73c024750949d
airinsanish <110320058+airinsanish@users.noreply.github.com>
2022-07-31 09:49:13 +0530

    Add files via upload

---
+++ perc_calc.py
@@ -0,0 +1,22 @@
+a = int(input("Enter the mark of Malayalam:"))
+b = int(input("Enter the mark of English:"))
+c = int(input("Enter the mark of Maths:"))
+d = int(input("Enter the mark of Science:"))
+e = int(input("Enter the mark of Social:"))
+f = int(input("Enter the mark of Hindi:"))
+g = int(input("Enter the mark of Gk:"))
+h = int(input("Enter the mark of Computer:"))
+i = int(input("Enter the mark of Value education:"))
+total = (a+b+c+d+e+f+g+h+i)
+Total = print(("Total="),a+b+c+d+e+f+g+h+i)
+max = int(input("Enter the Maximum mark of every subject ="))
+avg = ((a+b+c+d+e+f+g+h+i)/9)
+print(avg)
+perc =int((total*100)/(max*9))
+print("percentage = ")
+print(perc)
+
+
+
+
+
